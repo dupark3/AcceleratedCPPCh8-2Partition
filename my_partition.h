fe15340cb100f2023c77ea4bdc08f1d877250530
Du Park <dupark3@gmail.com>
2018-03-01 17:06:47 -0500

    First working version of Partition

---
+++ my_partition.h
@@ -0,0 +1,9 @@
+#ifndef GUARD_my_partition_h
+#define GUARD_my_partition_h
+
+#include "my_partition.hpp"
+
+template <class outputIterator, class predicate>
+outputIterator my_partition(outputIterator, outputIterator, predicate);
+
+#endif // GUARD_my_partition_h
---
+++ my_partition.hpp
@@ -0,0 +1,18 @@
+
+
+template <class outputIterator, class predicate>
+outputIterator my_partition(outputIterator first, outputIterator last, predicate pred){
+    outputIterator it = first;
+    while (it != last){
+        if (pred(*it)){
+            *last = *first; // store this false value in last temporarily
+            *first = *it;
+            *it = *last;
+            ++first;
+            ++it;
+        } else {
+            ++it;
+        }
+    }
+
+}
