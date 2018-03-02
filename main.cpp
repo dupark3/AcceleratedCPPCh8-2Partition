345b140f13f2d67d426b1f9b146bd1f5bbf16617
Du Park <dupark3@gmail.com>
2018-03-02 08:23:55 -0500

    Prints just the first half using the returned iterator

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ bool IsEven(int n){
 }
 
 int main(){
-    int myints[] = {1, 2, 3, 4, 5};
+    int myints[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
     std::vector<int> vec (myints, myints + sizeof(myints) / sizeof(int));
 
     // print the original vector
@@ -19,10 +19,15 @@ int main(){
     std::cout << std::endl;
 
     // put even numbers at the front and odd numbers at the back
-    my_partition(vec.begin(), vec.end(), IsEven);
+    std::vector<int>::const_iterator it = my_partition(vec.begin(), vec.end(), IsEven);
     std::cout << "The result of partitioning even numbers: ";
     for (std::vector<int>::size_type i = 0; i != vec.size(); ++i)
         std::cout << vec[i] << " ";
+
+    // the returned iterator points to the beginning of the partition. We can use it to print just the first half.
+    std::cout << std::endl << "Using the returned iter to show first half: ";
+    for(std::vector<int>::const_iterator i = vec.begin(); i != it; ++i)
+        std::cout << *i << " ";
 
     return 0;
 }
